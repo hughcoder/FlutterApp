9387ac7c24037df3d70b06201b03cac2ad2ab4d0
chenyw <chenyw@tuya.com>
2018-10-16 18:12:39 +0800

    Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(new MyApp());
 
@@ -7,25 +8,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter rolling demo'),
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+          primarySwatch: Colors.blue,
         ),
-        body: Center(child: RollingButton(),),
-      )
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter rolling demo'),
+          ),
+          body: Center(
+            child: TextWidget(),
+          ),
+        ));
+  }
+}
 
+class TextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Put your text here",
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold
+      ),
     );
   }
 }
@@ -39,6 +54,8 @@ class RollingButton extends StatefulWidget {
 }
 
 class _RollingState extends State<RollingButton> {
+  final _random = Random();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -48,14 +65,20 @@ class _RollingState extends State<RollingButton> {
     );
   }
 
+  List<int> _roll() {
+    final roll1 = _random.nextInt(6) + 1;
+    final roll2 = _random.nextInt(6) + 1;
+    return [roll1, roll2];
+  }
+
   void _onPressed() {
     debugPrint('_onPressed'); //用commond+4打开
-
+    final rollResults = _roll();
     showDialog(
         context: context,
         builder: (_) {
           return AlertDialog(
-            content: Text('AlertDialog'),
+            content: Text('Roll result:(${rollResults[0]},${rollResults[1]})'),
           );
         });
   }
